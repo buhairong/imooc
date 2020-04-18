894dee54d18a7591d9c28bb222005a9a8b0224cf
buhairong <22858111@qq.com>
2020-04-18 19:50:41 +0800

    11

---
+++ uniapp/lianxi/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="box" hover-class="hoverclass">
+		<text>测试文字</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.box {
+		border: 1px solid red;
+	}
+	.hoverclass {
+		background:#007AFF;
+	}
+</style>
