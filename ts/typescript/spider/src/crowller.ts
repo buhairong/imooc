76b9674763f9bfdba45289cd2ec98363781cba73
buhairong <22858111@qq.com>
2021-01-18 22:02:20 +0800

    1

--- ts/typescript/spider/src/crowller.ts
+++ ts/typescript/spider/src/crowller.ts
@@ -1,71 +1,38 @@
 // superagent 是 js文件，ts无法直接读取
 // ts -> .d.ts 翻译文件 -> js
 import superagent from 'superagent'
-import cheerio from 'cheerio'
 import fs from 'fs'
 import path from 'path'
+import DellAnalyzer from './dellAnalyzer'
 
-interface Course {
-  title: string,
-  count: number
-}
-
-interface CourseResult {
-  time: number,
-  data: Course[]
-}
-
-interface Content {
-  [propName: number]: Course[]
+export interface Analyzer {
+  analyzer: (html: string, filePath: string) => string
 }
 
 class Crowller {
-  private secret = 'x3b174jsx'
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private filePath = path.resolve(__dirname, '../data/course.json')
 
-  constructor() {
+  constructor(private url: string, private analyzer: Analyzer) {
     this.initSpiderProcess()
   }
 
-  async initSpiderProcess() {
-    const filePath = path.resolve(__dirname, '../data/course.json')
+  private async initSpiderProcess() {
     const html = await this.getRawHtml()
-    const courseResult = this.getCourseInfo(html)
-    this.generateJsonContent(courseResult)
-    fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyzer(html, this.filePath)
+
+    fs.writeFileSync(this.filePath, fileContent)
   }
 
-  async getRawHtml() {
+  private async getRawHtml() {
     const result = await superagent.get(this.url)
     return result.text
   }
-
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    const courseInfos: Course[] = []
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1])
-      courseInfos.push({title, count})
-    })
-
-    return {
-      time: new Date().getTime(),
-      data: courseInfos
-    }
-  }
-
-  generateJsonContent(courseInfo: CourseResult) {
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    let fileContent: Content = {}
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    } 
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
-  }
 }
 
-const crowller = new Crowller()
+const secret = 'x3b174jsx'
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+
+
+const analyzer = DellAnalyzer.getInstance()
+new Crowller(url, analyzer)
+
