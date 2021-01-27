b4f36d41a5c80599a265fbaf94fabb6c24e9afd3
hairong.pu.o <hairong.pu.o@nio.com>
2021-01-27 19:53:59 +0800

    1

--- ts/typescript/decorator/src/index.ts
+++ ts/typescript/decorator/src/index.ts
@@ -1,6 +1,7 @@
 // 类的装饰器
 // 装饰器本身是一个函数
 // 装饰器通过 @ 符号来使用
+// 多个装饰器从下至上，从右至左执行
 
 // 装饰器接收的参数为类的构造函数
 function testDecorator(constructor: any) {
@@ -10,7 +11,19 @@ function testDecorator(constructor: any) {
   console.log('decorator')
 }
 
+// 工厂模式
+function testDecorator1(flag: boolean) {
+  if(flag) {
+    return function(constructor: any) {
+      console.log('decorator1')
+    }
+  }else {
+    return function(){}
+  }
+}
+
 @testDecorator
+@testDecorator1(true)
 class Test {
 
 }
