6054e398aff3f49ba62ec38c96478f2f86cd537d
hairong.pu.o <hairong.pu.o@nio.com>
2021-01-28 18:54:01 +0800

    1

--- ts/typescript/decorator/src/index.ts
+++ ts/typescript/decorator/src/index.ts
@@ -1,32 +1,45 @@
-// 类的装饰器
-// 装饰器本身是一个函数
-// 装饰器通过 @ 符号来使用
-// 多个装饰器从下至上，从右至左执行
+// function testDecorator() {
+//   return function<T extends new (...args: any[]) => {}>(constructor: T) {
+//     return class extends constructor {
+//       name = 'lee'
+//       getName() {
+//         return this.name
+//       }
+//     }
+//   }
+// }
 
-// 装饰器接收的参数为类的构造函数
-function testDecorator(constructor: any) {
-  constructor.prototype.getName = () => {
-    console.log('dell')
+// const Test = testDecorator()(class {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+// })
+
+// 普通方法：target 对应的是 类的 prototype , key 是 装饰的方法的名字, descriptor 可以对 方法的descriptor属性做一些编辑
+// 静态方法：target 对应的是 类的 构造函数
+function getNameDecorator(target: any, key: string, descriptor: PropertyDescriptor) {
+  //descriptor.writable = false
+  descriptor.value = function() {
+    return 'decorator'
   }
-  console.log('decorator')
 }
 
-// 工厂模式
-function testDecorator1(flag: boolean) {
-  if(flag) {
-    return function(constructor: any) {
-      console.log('decorator1')
-    }
-  }else {
-    return function(){}
-  }
-}
-
-@testDecorator
-@testDecorator1(true)
 class Test {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
 
+  @getNameDecorator
+  getName() {
+    return this.name
+  }
 }
 
-const test = new Test();
-(test as any).getName()
+
+const test = new Test('dell');
+console.log(test.getName())
+
+
+
